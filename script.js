3ce57bb2547add8667a83039c07d931132888862
davidIvankov <david.ivankov96@gmail.com>
2023-04-25 05:33:15 +0200

    books array updating

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
+const title = document.getElementById('title'),
+author = document.getElementById('author'),
+red = document.getElementById('red'),
+submit = document.getElementById('submit-btn'),
+container = document.getElementById('container')
+validateTitle = function(){
+    if (!title.value.trim() || !author.value.trim()){
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+},
+validateAuthor = function(){
+    if (!author.value.trim() || !title.value.trim()){
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+};
 
+title.onkeyup = validateTitle;
+author.onkeyup = validateAuthor;
 
 let books = [];
 class Book{
 
-    constructor(title, author){
+    constructor(title, author, red){
         this.title = title;
-        this.author = author;    
+        this.author = author;
+        this.red = red    
    };
+   set red(value){
+    if (typeof value !== 'boolean'){
+        console.log('fail')
+    } else {
+        this._red = value
+    }
+   }
+   get red() {
+    return this._red
+   }
     set author(value){
     if (!value.trim() || typeof value !== 'string'){
             console.log('nece ici')
@@ -39,12 +71,19 @@ class Book{
 
 }
 
-let jure = new Book('jeo','  dasdsa ');
+
 
 function addBook(value){
-    if (value instanceof Book && value.author ){
+    if (value instanceof Book){
         books.push(value)
     } else {
         console.log('fail')    }
 
 }
+submit.addEventListener('click',(event)=>{
+    console.log(red.value)
+    addBook(new Book(title.value, author.value, red.checked))
+    
+
+    event.preventDefault()
+})
